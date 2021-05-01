0a30980a931c3d50d08a40879bf448dd32ac2740
Ayush Sanklecha <asanklecha@teksystems.com>
2021-05-01 12:59:01 +0530

    Added redux and implemented user reducer and actions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
+import Store from './redux/store';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={Store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
