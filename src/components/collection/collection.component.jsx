edc08ed1f3072608a1049aab1ce922ba0e8feb1d
Ayush Sanklecha <asanklecha@teksystems.com>
2021-05-03 16:35:52 +0530

    updated cart utils to group items

--- src/components/collection/collection.component.jsx
+++ src/components/collection/collection.component.jsx
@@ -7,9 +7,11 @@ const Collection = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.filter((item,idx) => idx < 4).map(({id, ...OtheritemProps}) => (
-        <CollectionItem key={id} {...OtheritemProps}/>
-      ))}
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))} 
     </div>
   </div>
 );
